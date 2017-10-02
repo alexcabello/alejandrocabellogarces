b90c367cf0b362f6e94f825d4ded063dbf6c751a
Maria <Maria@Alex-PC>
2017-10-02 17:00:10 +0200

    esborrat funció fantasma

--- index.php
+++ index.php
@@ -26,10 +26,7 @@ and open the template in the editor.
                 return "23";
             }
             
-            public function fantasma() {
-                return "*11*";
-            }
-            
+          
             
         }
         
